b96ee5747d7e2154ae7848e57d6cb0badb6da4b8
milindkesar <milindkesar@gmail.com>
2020-06-27 23:34:24 +0530

    routes for nav bar + base html file

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request,'main/index.html')
+def login(request):
+    return render(request, 'main/login.html')
+def marketplace(request):
+    return render(request, 'main/Marketplace.html')
+def blog(request):
+    return render(request, 'main/blog.html')
