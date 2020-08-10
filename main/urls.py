6fb0187bda01c96ed81222dd5e7048853e012e5a
SiddharthPh <51734363+SiddharthPh@users.noreply.github.com>
2020-08-10 23:23:53 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,5 @@ urlpatterns=[
     path('login/',views.login,name="login"),
     path('marketplace/',views.marketplace,name="marketplace"),
     path('blogs/',views.blog,name="blog"),
-    path('contactus/',views.contact_us,name="contact_us"),
-    path('submit',views.urlview,name="url_form")
+    path('contactus/',views.contact_us,name="contact_us")
 ]
