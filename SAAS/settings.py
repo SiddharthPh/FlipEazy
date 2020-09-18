93456f62ce32a1584bd70f93823c8a1cdc6930c6
Siddhartha Pothukuchi <siddharthapothukuchi@gmail.com>
2020-09-18 16:25:17 +0530

    update

--- SAAS/settings.py
+++ SAAS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "9db1683df3327b09301c58e47d8b83f34686d92eef04c042"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
