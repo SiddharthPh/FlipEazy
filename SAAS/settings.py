49ddc3ece5ef9754b60074b9ae39b54058baf526
Siddhartha Pothukuchi <siddharthapothukuchi@gmail.com>
2020-09-18 15:09:31 +0530

    Second2

--- SAAS/settings.py
+++ SAAS/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
