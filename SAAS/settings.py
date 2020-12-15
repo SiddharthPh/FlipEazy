3a0d28ff41c63e59cccb19157deedc40c37a4dd0
Siddhartha Pothukuchi <siddharthapothukuchi@gmail.com>
2020-12-16 05:22:01 +0530

    final_1.2

--- SAAS/settings.py
+++ SAAS/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_on_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,6 +28,7 @@ SECRET_KEY = "9db1683df3327b09301c58e47d8b83f34686d92eef04c042"
 DEBUG = True
 
 ALLOWED_HOSTS = ['saasmarketplace.herokuapp.com','127.0.0.1']
+django_on_heroku.settings(locals())
 
 
 # Application definition
